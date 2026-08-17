85149a9670e29314f105d5e4530c969c0e0e7eb8
caioosants <gilkamesh8@gmail.com>
2026-08-17 17:01:40 -0300

    Criar estrutura inicial do projeto

---
+++ src/main/java/br/com/meuestoque/meuestoque/MeuestoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuestoque.meuestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuestoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuestoque/meuestoque/MeuestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuestoque.meuestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
